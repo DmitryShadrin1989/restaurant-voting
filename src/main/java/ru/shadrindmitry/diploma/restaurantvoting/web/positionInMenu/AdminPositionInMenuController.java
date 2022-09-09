4880fcfe95ef15b1ddc59712356c83c1acab2b21
DmitryShadrin1989 <104692130+DmitryShadrin1989@users.noreply.github.com>
2022-09-09 14:15:50 +0300

    Added spring security functionality to the PositionInMeny entity

--- src/main/java/ru/shadrindmitry/diploma/restaurantvoting/web/positionInMenu/AdminPositionInMenuController.java
+++ src/main/java/ru/shadrindmitry/diploma/restaurantvoting/web/positionInMenu/AdminPositionInMenuController.java
@@ -2,48 +2,24 @@ package ru.shadrindmitry.diploma.restaurantvoting.web.positionInMenu;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.shadrindmitry.diploma.restaurantvoting.model.PositionInMenu;
-import ru.shadrindmitry.diploma.restaurantvoting.repository.PositionInMenuRepository;
-import ru.shadrindmitry.diploma.restaurantvoting.service.PositionInMenuService;
 import ru.shadrindmitry.diploma.restaurantvoting.to.PositionInMenuTo;
 
 import java.net.URI;
-import java.time.LocalDate;
-import java.util.List;
 
-import static ru.shadrindmitry.diploma.restaurantvoting.util.PositionInMenuUtil.createTo;
-import static ru.shadrindmitry.diploma.restaurantvoting.util.PositionInMenuUtil.getTos;
 import static ru.shadrindmitry.diploma.restaurantvoting.util.validation.ValidationUtil.assureIdConsistent;
 import static ru.shadrindmitry.diploma.restaurantvoting.util.validation.ValidationUtil.checkNew;
 
 @RestController
-@RequestMapping(value = PositionInMenuController.REST_URL)
+@RequestMapping(value = AdminPositionInMenuController.REST_URL)
 @Slf4j
 @AllArgsConstructor
-public class PositionInMenuController {
-    static final String REST_URL = "/api/menu";
-
-    protected PositionInMenuService positionInMenuService;
-    protected PositionInMenuRepository positionInMenuRepository;
-
-    @GetMapping("{id}")
-    public PositionInMenuTo get(@PathVariable int id) {
-        log.info("get PositionInMenu {}", id);
-        return createTo(positionInMenuRepository.getExisted(id));
-    }
-
-    @GetMapping
-    public List<PositionInMenuTo> getRestaurantMenu(@RequestParam int restaurant_id,
-                                    @RequestParam @Nullable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        log.info("getAll PositionInMenu for Restaurant {} on date {}", restaurant_id, date);
-        return getTos(positionInMenuService.getRestaurantMenu(restaurant_id, date));
-    }
+public class AdminPositionInMenuController extends AbstractPositionInMenuController {
+    static final String REST_URL = "/api/admin/menu";
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
